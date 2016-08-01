f5e2e26a29b296038ff8655c125d68ffd785840e
Jaime Jean Claude Quintero Arreola <jaime.quintero@softtek.com>
2016-08-01 15:06:28 -0700

    Change css

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'new-libraryapp',
+    podModulePrefix: 'new-libraryapp/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
